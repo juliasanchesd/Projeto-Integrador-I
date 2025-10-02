9823915563307fac0f17002e9c7acf73c8d157ed
Júlia <juliasanchesdourado@gmail.com>
2025-10-02 17:33:34 -0300

    reorganize project folders

--- Módulo_2-Análise_dos_dados/backend/src/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI  
-
-# Cria a aplicação
-app = FastAPI()
-
-# Define uma rota para a página inicial
-@app.get("/")
-def ler_raiz():
-    return {"Olá": "Mundo no PowerShell!"}
